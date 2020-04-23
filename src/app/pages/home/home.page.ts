aaf62b56fbda216427f08c26af9d64b574afef9b
songjiazhun <869077613@qq.com>
2020-04-23 09:54:26 +0800

    bug

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { StorageService } from '../../services/storage.service';
+import { StorageService } from '../../services/storage.service';
+declare let appManager: AppManagerPlugin.AppManager;
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -7,16 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
-  //constructor(private storageServicer:StorageService) { }
+  //constructor() { }
+  constructor(private storageServicer:StorageService) { }
 
   ngOnInit() {
-    // this.storageServicer.getTest().then((data)=>{
-    //     console.log("==msg=="+JSON.stringify(data));
-    //     console.log("==msg=="+JSON.stringify(data));
-    // }).catch((err)=>{
-    //   alert(JSON.stringify(err));
-    // })
+     this.storageServicer.getTest().then((data)=>{
+         console.log("==msg=="+JSON.stringify(data));
+         console.log("==msg=="+JSON.stringify(data));
+         if(data === null){
+
+         }
+     }).catch((err)=>{
+       alert(JSON.stringify(err));
+     })
+  }
+
+  ionViewDidEnter(){
+    // When the main screen is ready to be displayed, ask the app manager to make the app visible,
+    // in case it was started hidden while loading.
+    appManager.setVisible("show");
+
   }
 
 }
