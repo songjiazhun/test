af388d44047cf2324b0eb55b61cc9c0e35f46aea
songjiazhun <869077613@qq.com>
2020-04-23 17:32:52 +0800

    add ...

---
+++ src/assets/i18n/en.ts
@@ -0,0 +1,10 @@
+export const en = {
+  "homePage":{
+    "pageTitle":"Home",
+    "testKey":"set",
+    "testKey1":"get",
+    "testKey2":"remove",
+    "testKey3":"clear",
+    "testKey4":"setLangule",
+   }
+};
---
+++ src/assets/i18n/zh.ts
@@ -0,0 +1,10 @@
+export const zh = {
+   "homePage":{
+    "pageTitle":"首页",
+    "testKey":"设置",
+    "testKey1":"获取",
+    "testKey2":"移除",
+    "testKey3":"清空",
+    "testKey4":"设置语言",
+   }
+};
